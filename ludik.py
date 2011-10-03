bf6d5b51618b9b3cfa8fb21b0c80df14f1922aa7
Maksim Gruzdev <astro64m@gmail.com>
2011-10-03 21:34:49 +0400

    switch file implemented

--- ludik.py
+++ ludik.py
@@ -1,7 +1,10 @@
-import sublime, sublime_plugin  
+import sublime, sublime_plugin, os
 
 class LudikMoveCommand(sublime_plugin.TextCommand):
+	"""seeks and sets cursor to the beggining of standart eludia screen function"""
+
 	def run(self, edit, action): 
+
 		action_folder = {
 			'get_item_of'  : 'Content',
 			'select'       : 'Content',
@@ -11,21 +14,44 @@ class LudikMoveCommand(sublime_plugin.TextCommand):
 			'draw_item_of' : 'Presentation',
 			'draw'         : 'Presentation'
 		}
-		self.__switch_to(action_folder[action])
-		self.goto_sub('sub ' + action + '_' + self.__currentScreenType())
 
-	def goto_sub(self, subname):
+		self.opened_view = self.__switch_to(action_folder[action])
+		self.search_for_sub = 'sub ' + action + '_' + self.__currentScreenType()
+		sublime.set_timeout(self.__seek_if_view_loaded, 250)
 
-		pt = self.view.find(subname, 0)
+	def __seek_if_view_loaded(self):
+		if self.opened_view.is_loading():
+			sublime.set_timeout(self.__seek_if_view_loaded, 250)
+			return
+		
+		sublime.status_message(self.search_for_sub)
+		self.__goto_sub(self.opened_view, self.search_for_sub)
 
+	def __goto_sub(self, view, subname):
+		
+		pt = view.find(subname, 0)
+		
 		if pt:
-			self.view.sel().clear()
-			self.view.sel().add(pt.begin())
+			view.sel().clear()
+			view.sel().add(pt.begin())
 
-			self.view.show(pt.begin())
+			view.show(pt.begin())
 	
+		
 	def __currentScreenType(self):
-		pass
+		file_name = os.path.basename(self.view.file_name())
+		return os.path.splitext(file_name)[0]
 
-	def __switch_to(self, folder):
-		pass
+	def __switch_to(self, target_folder):
+		
+		current_screen_folder = os.path.dirname(self.view.file_name())
+		
+		lib_dir = os.path.dirname(current_screen_folder)
+		new_file = os.path.join(lib_dir, target_folder, self.__currentScreenType()) + '.pm'
+
+		if not os.path.exists(new_file):
+			sublime.status_message('file ' + new_file + ' does not exist')
+			return
+
+		return self.view.window().open_file(new_file)
+
